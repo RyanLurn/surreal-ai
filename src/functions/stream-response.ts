4811be6fb0cec191d3d7feb1669dc91258abe303
Ryan Luong <lumintriet@gmail.com>
2025-09-24 21:28:59 +0700

    Stream AI response function.

---
+++ src/functions/stream-response.ts
@@ -0,0 +1,28 @@
+import { streamText } from "ai";
+import { createMessage } from "@/functions/create-message";
+import { listMessages } from "@/functions/list-messages";
+import { groqProvider } from "@/lib/ai-providers";
+
+async function streamResponse({
+  updateStream,
+}: {
+  updateStream: (textPart: string) => void;
+}) {
+  const messages = await listMessages();
+
+  const { textStream, text } = streamText({
+    messages,
+    model: groqProvider("llama-3.1-8b-instant"),
+  });
+
+  for await (const textPart of textStream) {
+    updateStream(textPart);
+  }
+
+  await createMessage({
+    content: await text,
+    role: "assistant",
+  });
+}
+
+export { streamResponse };
