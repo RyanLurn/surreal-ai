69fa3e923f631b5b4e751c01bede3c50f83d538e
Ryan Luong <lumintriet@gmail.com>
2025-09-24 19:52:03 +0700

    MessageAvatar component.

---
+++ src/components/chat/message/avatar.tsx
@@ -0,0 +1,12 @@
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+
+function MessageAvatar({ role }: { role: "user" | "assistant" }) {
+  return (
+    <Avatar className="size-10 ring ring-border">
+      {/* TODO: Support avatar's image */}
+      <AvatarFallback>{role === "user" ? "You" : "AI"}</AvatarFallback>
+    </Avatar>
+  );
+}
+
+export { MessageAvatar };
